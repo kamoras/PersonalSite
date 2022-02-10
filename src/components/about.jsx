3939b7579dd1fd9b0ff4395ae6945159b171e831
Ryan Mack <rmack@datto.com>
2022-02-09 23:10:45 -0500

    Working portfolio site

---
+++ src/components/about.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                        <span className="heading-meta">About Us</span>
+                        <h2 className="colorlib-heading">Who Am I?</h2>
+                        <p>I'm Ryan, a senior software engineer at Datto with a passion for democratizing tech.</p>
+                        <p>Recently, I have been working on data backup and recovery software in C++, Python, and PHP. This software is designed to be a turn-key solution, making it simple for small and medium businesses to protect their data in an increasingly dangerous cyber landscape.</p>
+                        <p>In the past, I have worked at PerkinElmer where I created a prototype of how to transition the traditional ICP-OES instruments to a cloud backend infrastructure to allow scientists of any technical ability to quickly and easily share data from experiments with their peers. I also worked at Thermo Fisher Scientific where I worked on genetic sequencing instruments. The goal was to take a multiday workflow that involved multiple instruments down to one single 8-hour workflow using a single machine. I believe that simplicity is key when designing software for my customers.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Skills & Expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Backend Development</h3>
+                    <p>I have experience developing backend applications in C++, Python, Java, and PHP</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Data Structures & Algorithms</h3>
+                    <p>Coming from the CS background, I have strong grasp of the fundamental concepts of DSA</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Low Level Development</h3>
+                    <p>Experience with "low level" development, including filesystems, kernel drivers, robotics, and more.</p>
+                </div>
+                </div>
+            </div>
+            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Senior Software Engineer at Datto <span>Norwalk, Connecticut (October 2019 - Present)</span></h2>
+                        <p>Datto (NYSE: MSP) is a leader in supporting MSPs (managed service providers) in their mission to support small and medium businesses' IT needs. As a member of the BCDR (Business Continuity and Data Recovery) agents team, I maintain the software which performs backups of protected systems. A key concern for the project is creating fault tolerant, secure, cross-platform software (in modern C++) that will reliably create backups for our customers. Innovation is key as we continue to lead this growing industry.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Development Engineer at PerkinElmer <span>Shelton, Connecticut (October 2018 - October 2019)</span></h2>
+                        <p>As a software engineer at PerkinElmer I worked on a desktop app for Windows, called Syngistix. Development was done in C# and C++. We followed agile and TDD practices. Lots of work involved updating a legacy software product into something more modern. </p>
+                        <p>Creator of the first PoC for PerkinElmer instrument cloud platform. I worked with minimal requirements and help, creating a first of its kind instrument cloud within the company. I learned many new technologies for cloud development, such as: kubernetes, docker, python flask, and node.js. I also taught my team how to use these technologies as I was learning them. </p>
+                        <p>While working to modernize our software and tools, I was a leader in migrating to GitHub and TeamCity from Perforce and Jenkins.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Staff Software Engineer at Capgemini Engineering <span>Burlington, Massachusetts (June 2018 - October 2018)</span></h2>
+                        <p>I worked on projects for ASML in Wilton, CT, focusing on embedded programming. The primary product that I worked on was EUV (extreme ultraviolet) lithography. </p>
+                        <p>Languages used: C++ and Python. </p>
+                        <p>Wrote software to improve the function of the "top" of ASML lithography devices. The top is responsible for the handling of the reticle, a piece of glass that light is passed through to etch the design of a semiconductor chip into a silicon wafer. These reticles must be moved in a clean environment within the device through complex robotics in order to prevent contamination from dust (which would ruin the pattern being etched).</p>
+                        <p>Key challenges: process oriented development, involving systems that are NOT allowed to fail. Even the smallest change must undergo intense scrutiny.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Engineer II at Thermo Fisher Scientific <span>Guilford, Connecticut (May 2017 - June 2018)</span></h2>
+                        <p>Full time member of the team maintaining genetic sequencing devices at the Ion Torrent division of Thermo Fisher. </p>
+                        <p>Used Java to create robust user experiences and provide localization for use of our devices around the globe. </p>
+                        <p>Maintained a C++ backend and consistently cut away at bottlenecks in the system since DNA sequencing is typically a very slow operation. Our next generation product is capable to running an entire sequence during a single work shift (8 hours), which is previously unheard of. </p>
+                        <p>Selected Projects:</p>
+                        <p>Ion Genestudio S5 Series software: Contributed regularly to the maintenance of this product lines including demo software for trade shows and regular bug fixes.</p>
+                        <p>Genexus Integrated Sequencer: Worked on the instrument software for the very first Genexus prototype. This included C++ code for RFID tag reader, automated pipette robots, and a complex microfluidics system.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Engineer Intern <span>Guilford, Connecticut (May 2016 - August 2016)</span></h2>
+                        <p>Internship for the summer of 2016 working with a team of about 5 people maintaining the software for genetic sequencing devices by Ion Torrent</p>
+                        <p>My responsibilities involved software development, troubleshooting technical issues with the machines, and the testing of both new hardware and software. During this time, I improved my skills in C++, Java, Python, and Linux. </p>
+                        <p>Selected accomplishments: </p>
+                        <p> - Researched, developed and delivered an on-screen keyboard in both Chinese and German.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
