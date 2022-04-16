8fde439b9e837c411ed82c5da8b267590418ffd7
Ryan Mack <mack.ryanm@gmail.com>
2022-04-15 23:11:51 -0400

    update about me to say thousandeyes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,9 +13,10 @@ export default class About extends Component {
                     <div className="about-desc">
                         <span className="heading-meta">About Us</span>
                         <h2 className="colorlib-heading">Who Am I?</h2>
-                        <p>I'm Ryan, a senior software engineer at Datto with a passion for democratizing tech.</p>
-                        <p>Recently, I have been working on data backup and recovery software in C++, Python, and PHP. This software is designed to be a turn-key solution, making it simple for small and medium businesses to protect their data in an increasingly dangerous cyber landscape.</p>
-                        <p>In the past, I have worked at PerkinElmer where I created a prototype of how to transition the traditional ICP-OES instruments to a cloud backend infrastructure to allow scientists of any technical ability to quickly and easily share data from experiments with their peers. I also worked at Thermo Fisher Scientific where I worked on genetic sequencing instruments. The goal was to take a multiday workflow that involved multiple instruments down to one single 8-hour workflow using a single machine. I believe that simplicity is key when designing software for my customers.</p>
+                        <p>I'm Ryan, a senior software engineer at ThousandEyes (part of Cisco) with a passion for democratizing tech.</p>
+                        <p>My current work involves the Enterprise Agent at ThousandEyes. This software is designed to be a turn-key solution, which makes it simple for users to understand and react to network issues no matter where they are. This is increasingly important as companies transition away from traditional on-premise servers to the cloud (where visibility would normally be limited).</p>
+                        <p>In my previous role, I worked at Datto, where I developed software to backup and secure small and medium businesses. Before that, at PerkinElmer, I created a proof of concept to transition the traditional ICP-OES instrument platform to a cloud-based design. I also worked at Thermo Fisher Scientific where I worked on genetic sequencing instruments. The goal was to take a multiday workflow that involved multiple instruments down to one single 8-hour workflow using a single machine.</p>
+                        <p>I believe that simplicity is key when designing software for my customers.</p>
                     </div>
                 </div>
                 </div>
