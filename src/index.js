c918b700d168228d5e5078eebb655fb61381d559
Ryan Mack <45410074+kamoras@users.noreply.github.com>
2025-06-06 15:31:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const div = document.createElement('div');
+const root = createRoot(div); // New API
+
+root.render(<App />);
+root.unmount(); // Unmount the component
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
